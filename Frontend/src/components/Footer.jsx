2dfc835a1222487bea5bf724c89569e200f9e37f
Ravikiran9988 <vk8896992@gmail.com>
2025-08-31 14:54:37 +0530

    Delete Frontend directory

--- Frontend/src/components/Footer.jsx
+++
@@ -1,3 +0,0 @@
-const Footer = () => { return ( <footer className="text-center p-6 bg-white shadow-md"> <p className="text-gray-700">© 2025 Radiant Skincare | Stay Beautiful!</p> </footer> ); };
-
-export default Footer;
--- Frontend/src/pages/Analysis.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SkinAnalysis from "../components/SkinAnalysis";
-
-const Analysis = () => {
-  return <SkinAnalysis />;
-};
-
-export default Analysis;
